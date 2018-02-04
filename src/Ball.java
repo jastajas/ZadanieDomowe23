df3f218253a25f2868d0abcd762f173f41c8e6d7
jackS <jastajas@gamil.com>
2018-02-04 22:33:15 +0100

    Klasa Ball - ver.1

---
+++ src/Ball.java
@@ -0,0 +1,10 @@
+public class Ball extends Shape3D {
+
+    public Ball(double dimensionX) {
+        super(dimensionX);
+    }
+
+    public Ball(double dimensionX, double area, double volume) {
+        super(dimensionX, area, volume);
+    }
+}
