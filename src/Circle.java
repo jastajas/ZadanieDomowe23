e5a9300bca67254d52c4c1078ada6a085fd75889
jackS <jastajas@gamil.com>
2018-02-04 22:34:44 +0100

    Klasa Circle - ver.1

---
+++ src/Circle.java
@@ -0,0 +1,13 @@
+public class Circle extends GeometricShape {
+
+    public Circle(double dimensionX) {
+        super(dimensionX);
+    }
+
+    public Circle(double dimensionX, double area, double circumference) {
+        super(dimensionX, area, circumference);
+    }
+
+
+
+}
