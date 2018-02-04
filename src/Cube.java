ad85c3410f1b63775330f3f6b0f5c778f54f14c2
jackS <jastajas@gamil.com>
2018-02-04 22:32:45 +0100

    Klasa Cube - ver.1

---
+++ src/Cube.java
@@ -0,0 +1,10 @@
+public class Cube extends Shape3D {
+
+    public Cube(double dimensionX) {
+        super(dimensionX);
+    }
+
+    public Cube(double dimensionX, double area, double volume) {
+        super(dimensionX, area, volume);
+    }
+}
