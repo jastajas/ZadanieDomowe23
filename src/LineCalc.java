7d98a2317b64107ab90b5fe9bd253d3503b18f24
jackS <jastajas@gamil.com>
2018-02-04 22:38:54 +0100

    Klasa LineCalc - ver.1

---
+++ src/LineCalc.java
@@ -0,0 +1,7 @@
+public class LineCalc {
+
+    public void lineLength(Line2D line){
+        System.out.printf("Długość odcinka wynosi [cm]: %.2f", line.getDimensionX());
+        System.out.println("");
+    }
+}
