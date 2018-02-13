c381cec652b578da641174836515233a6ad36523
jackS <jastajas@gamil.com>
2018-02-13 20:46:11 +0100

    Klasa Shape3D - ver.2

--- src/Shape3D.java
+++ src/Shape3D.java
@@ -1,44 +1,4 @@
-import java.util.Objects;
-
 public class Shape3D extends Shape{
-
-    private double area;
-    private double volume;
-
-    public Shape3D(double dimensionX, double area, double volume) {
-        super(dimensionX);
-        this.area = area;
-        this.volume = volume;
-    }
-
-
-    public Shape3D(double dimensionX) {
-        super(dimensionX);
-    }
-
-    public double getArea() {
-        return area;
-    }
-
-    public void setArea(double area) {
-        this.area = area;
-    }
-
-    public double getVolume() {
-        return volume;
-    }
-
-    public void setVolume(double volume) {
-        this.volume = volume;
-    }
-
-    @Override
-    public String toString() {
-        return "Shape3D{" +
-                "area=" + area +
-                ", volume=" + volume +
-                '}' + super.toString();
-    }
 }
 
 
