25d365e7213dd4d0f84ee085d1f0aed3bfeb8b9e
jackS <jastajas@gamil.com>
2018-02-13 20:45:41 +0100

    Klasa Shape - ver.2

--- src/Shape.java
+++ src/Shape.java
@@ -1,22 +1,2 @@
 public abstract class Shape {
-    private double dimensionX;
-
-    public Shape(double dimensionX) {
-        this.dimensionX = dimensionX;
-    }
-
-    public double getDimensionX() {
-        return dimensionX;
-    }
-
-    public void setDimensionX(double dimensionX) {
-        this.dimensionX = dimensionX;
-    }
-
-    @Override
-    public String toString() {
-        return "Shape{" +
-                "dimensionX=" + dimensionX +
-                '}';
-    }
 }
