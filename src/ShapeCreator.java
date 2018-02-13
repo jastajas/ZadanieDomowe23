ec34e7480445dad7959d2ce3f13ea181b28276f3
jackS <jastajas@gamil.com>
2018-02-13 22:49:03 +0100

    Interfejs ShapeCreator - ver.1 Nowy dodatkowy interfejs

---
+++ src/ShapeCreator.java
@@ -0,0 +1,7 @@
+public interface ShapeCreator {
+    public Line2D createLine();
+    public Circle createCircle();
+    public Rectangle createRectangle();
+    public Ball createBall();
+    public Cube createCube();
+}
